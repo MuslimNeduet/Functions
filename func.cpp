a6a917c212bad99047f288b7c9a8cee1310c7253
Muslimneduet <muslim.neduet@gmail.com>
2025-04-08 07:02:40 +0500

    Update func.cpp

--- func.cpp
+++ func.cpp
@@ -1,13 +1,14 @@
 #include<iostream>
 using namespace std;
 
-int sumtwo(int a, int b){
-    return a+b;
+int sumtwo(int a, int b){   //Takes Something   //Definition Of The Function
+    // a & b are default arguments
+    return a+b; //Returns Something
 }
 int main(){
     int x,y;
     cout<<"Creating a Function myself"<<endl;
     cout<<"Enter Your Numbers You Want To Add: "<<endl;
-    cin>>x>>y;
-    cout<<"The Sum Of Two Numbers You Entered Is: "<<sumtwo(x,y);
+    cin>>x>>y;      //Actual Arguments
+    cout<<"The Sum Of Two Numbers You Entered Is: "<<sumtwo(x,y);       //Calling Function
 }
